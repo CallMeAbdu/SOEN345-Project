5f61c4a3882737f71bae23e7b830f90236e455bf
Sofia Cimon <cimon.sofia@gmail.com>
2026-02-19 04:25:49 -0500

    codecov testing

--- app/src/test/java/com/soen345/project/ExampleUnitTest.java
+++ app/src/test/java/com/soen345/project/ExampleUnitTest.java
@@ -14,4 +14,9 @@ public class ExampleUnitTest {
     public void addition_isCorrect() {
         assertEquals(4, 2 + 2);
     }
+
+    public void second_dummy_test() {
+        assertEquals(10, 5 + 5);
+    }
+
 }
